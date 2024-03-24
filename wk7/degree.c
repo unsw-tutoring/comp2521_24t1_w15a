b50d626432c1e9d82703aefebb2b4274ab011cd9
Josh <joshconstantinelim@gmail.com>
2024-03-24 17:26:52 +1100

    wk7

---
+++ wk7/degree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "Graph.h"
+
+// return the degree of a vertex in a graph
+int degree(Graph g, int src) {
+    // TODO
+    return 0;
+}
